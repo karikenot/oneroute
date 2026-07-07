ad7cfbec1c9f8472613297e64b9ca70e03574f76
kari <iwanfadill37@gmail.com>
2026-07-07 12:48:44 +0800

    feat: adding the oneroutes table in the database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\OnerouteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [OnerouteController::class, 'index']);
