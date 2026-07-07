d5e8501f88cab3641e165c8ed2af7cd262a58b2d
rika <rikanabilarika7@gmail.com>
2026-07-07 15:51:49 +0800

    feat: modify href attribut in layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -19,7 +19,7 @@
         </div>
         <div class="navbar-end gap-2">
             <a href="#" class="btn btn-ghost btn-sm">Sign In</a>
-            <a href="#" class="btn btn-primary btn-sm">Sign Up</a>
+            <a href="/register" class="btn btn-primary btn-sm">Sign Up</a>
         </div>
     </nav>
 
