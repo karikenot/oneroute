787971b295f58cacaa5aa79e88dbba04e27e7a87
kari <iwanfadill37@gmail.com>
2026-07-07 14:48:25 +0800

    feat: adding the edit and delete button and adding routes in the web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [OnerouteController::class, 'index']);
 Route::post('/oneroutes', [OnerouteController::class, 'store']);
+Route::get('/oneroutes/{oneroute}/edit');
+Route::put('/oneroute/{oneroute}');
+Route::delete('/oneroute/{oneroute}');
